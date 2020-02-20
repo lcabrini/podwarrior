bbb89a3eadd865caba92e2a1ed21a3107bf1f72f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-02-20 17:06:07 +0000

    Initial work on parsing OMPL files.

---
+++ opml.c
@@ -0,0 +1,20 @@
+#include "opml.h"
+
+void parse_opml(char *fn)
+{
+    int i;
+
+    const xmlChar *up = (xmlChar *)"//@xmlUrl";
+    xmlDocPtr d = xmlParseFile(fn);
+    xmlXPathContextPtr c = xmlXPathNewContext(d);
+    xmlXPathObjectPtr u = xmlXPathEvalExpression(up, c);
+
+    for (i = 0; i < u->nodesetval->nodeNr; ++i) {
+        printf("%d. %s\n", i,
+                xmlNodeGetContent(u->nodesetval->nodeTab[i]));
+    }
+
+    xmlXPathFreeObject(u);
+    xmlXPathFreeContext(c);
+    xmlFreeDoc(d);
+}
